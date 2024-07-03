25c9a1916e94e4e6ebe3bed67116b30b91101fed
Andrew Howell <andrewthomashowell@gmail.com>
2024-07-03 22:11:21 +0100

    Cleaned up

--- main.go
+++ main.go
@@ -11,38 +11,59 @@ import (
 )
 
 type Config struct {
-	Starling Client `required:"true" envconfig:"starling"`
+	Starling *ClientConfig `required:"true" envconfig:"starling"`
 }
 
-type Client struct {
+type ClientConfig struct {
 	URL      string `required:"true" envconfig:"url"`
 	APIToken string `required:"true" envconfig:"api_token"`
 }
 
 func main() {
-	err := godotenv.Load(".env")
+	config, err := loadConfigFromEnv()
 	if err != nil {
 		panic(err)
 	}
 
-	config := &Config{}
-	envconfig.MustProcess("", config)
-
-	bearerToken := fmt.Sprintf("Bearer %s", config.Starling.APIToken)
-
-	c, err := starling.NewClientWithResponses(config.Starling.URL, starling.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
-		req.Header.Add("Authorization", bearerToken)
-		return nil
-	}))
+	starlingClient, err := newStarlingClient(config.Starling)
 	if err != nil {
 		panic(err)
 	}
 
-	resp, err := c.GetAccountsWithResponse(context.Background())
+	resp, err := starlingClient.GetAccountsWithResponse(context.Background())
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("getting Accounts: %w", err))
+	}
+	if resp.JSON4XX != nil {
+		panic(fmt.Errorf("getting Accounts: %+v", resp.JSON4XX))
 	}
 	for _, account := range *resp.JSON200.Accounts {
 		fmt.Println(fmt.Sprintf("%+v", account))
 	}
+}
+
+func loadConfigFromEnv() (*Config, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, fmt.Errorf("loading config from env: %w", err)
+	}
+
+	config := &Config{}
+	envconfig.MustProcess("", config)
+	return config, nil
+}
+
+func newStarlingClient(config *ClientConfig) (*starling.ClientWithResponses, error) {
+	bearerToken := fmt.Sprintf("Bearer %s", config.APIToken)
+	client, err := starling.NewClientWithResponses(
+		config.URL,
+		starling.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
+			req.Header.Add("Authorization", bearerToken)
+			return nil
+		}),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("initialising starling client: %w", err)
+	}
+	return client, nil
 }
