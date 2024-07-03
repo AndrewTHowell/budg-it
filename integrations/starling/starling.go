227c80f2b838aa9637d0c2c44187a7b242479f59
Andrew Howell <andrewthomashowell@gmail.com>
2024-07-03 21:38:48 +0100

    Using schema

--- integrations/starling/starling.go
+++ integrations/starling/starling.go
@@ -1,3 +1,3 @@
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml <path/to/swagger.json>
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml openapi.yaml
 
 package starling
