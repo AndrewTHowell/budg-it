886354bf18cbcb25bcac3f4f46ecc542fd21a99c
Andrew Howell <andrewthomashowell@gmail.com>
2024-07-16 00:41:34 +0100

    Append only schema

--- budgit/migrations/001_init.up.sql
+++ budgit/migrations/001_init.up.sql
@@ -1,6 +1,10 @@
 CREATE TABLE
   accounts (
-    id TEXT PRIMARY KEY,
+    request_id TEXT PRIMARY KEY,
+    valid_from_timestamp TIMESTAMPTZ,
+    valid_to_timestamp TIMESTAMPTZ,
+
+    id TEXT NOT NULL,
     name TEXT NOT NULL,
     cleared_balance BIGINT NOT NULL,
     effective_balance BIGINT NOT NULL,
@@ -10,18 +14,33 @@ CREATE TABLE
     external_integration_id TEXT,
     external_last_sync_timestamp TIMESTAMPTZ,
     external_cleared_balance BIGINT,
-    external_effective_balance BIGINT,
-    UNIQUE(external_id, external_integration_id)
+    external_effective_balance BIGINT
   );
 
-CREATE TABLE
-  payees (id TEXT PRIMARY KEY, name TEXT NOT NULL);
+CREATE INDEX accounts_request_id_idx ON accounts (request_id);
+CREATE INDEX accounts_id_idx ON accounts (id) WHERE valid_to_timestamp = 'infinity';
 
-CREATE INDEX payees_name_idx ON payees (name) INCLUDE (id);
+CREATE TABLE
+  payees (
+    request_id TEXT PRIMARY KEY,
+    valid_from_timestamp TIMESTAMPTZ,
+    valid_to_timestamp TIMESTAMPTZ,
+
+    id TEXT NOT NULL,
+    name TEXT NOT NULL
+  );
+
+CREATE INDEX payees_request_id_idx ON payees (request_id);
+CREATE INDEX payees_id_idx ON payees (id) WHERE valid_to_timestamp = 'infinity';
+CREATE INDEX payees_name_idx ON payees (name) WHERE valid_to_timestamp = 'infinity';
 
 CREATE TABLE
   transactions (
-    id TEXT PRIMARY KEY,
+    request_id TEXT PRIMARY KEY,
+    valid_from_timestamp TIMESTAMPTZ,
+    valid_to_timestamp TIMESTAMPTZ,
+
+    id TEXT NOT NULL,
     effective_date DATE NOT NULL,
     account_id TEXT NOT NULL,
     payee_id TEXT NOT NULL,
@@ -30,5 +49,7 @@ CREATE TABLE
     cleared BOOLEAN NOT NULL
   );
 
-CREATE INDEX transactions_account_effective_date_amount_idx ON transactions (account_id, effective_date, amount);
-CREATE INDEX transactions_effective_date_amount_idx ON transactions (effective_date, amount);
+CREATE INDEX transactions_request_id_idx ON transactions (request_id);
+CREATE INDEX transactions_id_idx ON transactions (id) WHERE valid_to_timestamp = 'infinity';
+CREATE INDEX transactions_account_effective_date_amount_idx ON transactions (account_id, effective_date, amount) WHERE valid_to_timestamp = 'infinity';
+CREATE INDEX transactions_effective_date_amount_idx ON transactions (effective_date, amount) WHERE valid_to_timestamp = 'infinity';
